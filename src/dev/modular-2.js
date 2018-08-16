62754cccd9f184b7ce62bc9dc50a243763107dd7
Jonas Karg <jonas.karg@icloud.com>
2018-08-16 11:55:01 +0200

    Minor changes/renaming

--- src/dev/modular-2.js
+++ src/dev/modular-2.js
@@ -1,5 +1,5 @@
 import { err, getAttr, getHtml, getStyle, makeEl } from "./core.js";
-import { getBinding, setBinding, listenBinding, scan, render } from "./direct-methods";
+import { getBinding, setBinding, listenBinding, scan, render } from "./methods";
 import errors from "./errors";
 import el from "./el";
 
@@ -7,15 +7,10 @@ const Modular = {
     // Events, bindings and error-messages
     data: {
         bindings: {},
-        renderedEvent: new Event("mRendered"),
-        onRender: new Event("mOnRender"),
-        errors: errors
+        renderedEvent: new Event("prerender"),
+        onRender: new Event("postrender"),
+        errors: errors || undefined
     },
-
-    // Declaration up here because the core relys on it
-    getBinding: getBinding,
-    setBinding: setBinding,
-    listenBinding: listenBinding,
 
     // The core
     core: {
@@ -26,12 +21,16 @@ const Modular = {
         makeEl: makeEl
     },
 
+    // Methods
+    getBinding: getBinding,
+    setBinding: setBinding,
+    listenBinding: listenBinding,
     scan: scan,
     el: el,
     render: render
 };
 
-// Making Modular global.
+// Making Modular a global constant
 Object.defineProperty(window, "Modular", {
     value: Modular,
     writable: false,
